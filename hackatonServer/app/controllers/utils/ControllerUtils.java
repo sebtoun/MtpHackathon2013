57479746755d5e3494f27828cfc27bec4d520edc
Hugo Des <hugo.deslongchamps@gmail.com>
2013-09-21 16:00:02 +0200

    working on http request for xml

--- hackatonServer/app/controllers/utils/ControllerUtils.java
+++ hackatonServer/app/controllers/utils/ControllerUtils.java
@@ -1,17 +1,53 @@
+package controllers.utils;
+
 import javax.xml.parsers.DocumentBuilder;
 import javax.xml.parsers.DocumentBuilderFactory;
 import javax.xml.parsers.ParserConfigurationException;
 import org.w3c.dom.Document;
+import java.io.IOException;
+import org.xml.sax.SAXException;
+import play.libs.F.Function;
+import play.libs.WS;
+import play.mvc.*; 
 
- 
- 
-// Creating a java DOM XML parser
-DocumentBuilderFactory builderFactory = DocumentBuilderFactory.newInstance();
-DocumentBuilder builder = null;
-builder = builderFactory.newDocumentBuilder();
+public class ControllerUtils {
 
-//Parsing xml with java dom parser
-String xml = ...;
-Document xmlDocument = builder.parse(new ByteArrayInputStream(xml.getBytes()));
+
+	public static String getDropOffs(float left, float bottom, float right, float top){
+
+
+		String requestURL = 
+			"http://api.openstreetmap.org/api/0.6/map?"
+			+left.toString()+","
+			+bottom.toString()+","
+			+right.toString()+","
+			+top.toString();
+
+		return xml(requestURL);
+
+		 
+
+
+		// Creating a java DOM XML parser
+		/*DocumentBuilderFactory builderFactory = DocumentBuilderFactory.newInstance();
+		DocumentBuilder builder = null;
+		try {
+		    builder = builderFactory.newDocumentBuilder();
+		} catch (ParserConfigurationException e) {
+		    e.printStackTrace();  
+		}
+		//Parsing xml with java dom parser
+		try {
+			Document xmlDocument = builder.parse(new ByteArrayInputStream(xml.getBytes()));
+		} catch (SAXException e) {
+		    e.printStackTrace();
+		} catch (IOException e) {
+		    e.printStackTrace();
+		}
+		*/	
+		
+
+	}
+}
 
 
