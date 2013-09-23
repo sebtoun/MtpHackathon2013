8612099dda1b2d8fd95ed07a36d2072164dd93d3
Kevin "Gaeel" Bradshaw-Rodriguez <k@xi.gd>
2013-09-23 13:53:52 +0200

    On trouve les points de collecte

--- hackatonServer/public/javascripts/app.web.js
+++ hackatonServer/public/javascripts/app.web.js
@@ -17,18 +17,44 @@ function getDropoffs(){
 		if (err) {
 			console.log("oops, pos not good")
 		} else {
-			console.log("Getting: " + "/geoloc/dropoffs/"+ pos.coords.latitude +"/" + pos.coords.longitude + "/10")
-			$.get("/geoloc/dropoffs/"+ pos.coords.latitude +"/" + pos.coords.longitude + "/20",
+			
+			//**
+			$.get("/geoloc/dropoffs/"+ pos.coords.latitude +"/" + pos.coords.longitude + "/1000",
 				{},
 				function(data, err) {
+					var dp;
+					var mindist = Infinity;
 					if (false){//err) {
-						console.log("oops, data not good")
+						console.log("oops, data not good");
 					} else {
-						console.log("Good?")
-						document.write(data);
+						console.log("Good?");
+						var dPoints = eval('(' + data + ')');
+						for (var i = dPoints.length - 1; i >= 0; i--) {
+							var dist = findDistance(pos,
+								{ coords: {
+									latitude: dPoints[i].latitude,
+									longitude: dPoints[i].longitude
+								}});
+							if (dist < mindist){
+								mindist = dist;
+								dp = dPoints[i];
+							}
+						};
+					}
+					if (dp) {
+						document.write("<p>Point de collecte à proximité:<br />" +
+							"Distance: " + Math.floor(mindist*100) + " mètres...<br />"+
+							"Latitude: " + dp.latitude + "°<br />"+
+							"Longitude: " + dp.longitude + "°<br />");
+
+
+						document.write("<a href=http://www.openstreetmap.org/#map=17/"+ dp.latitude +"/" + dp.longitude + ">Cliquer pour aller à la carte</a>")
+					} else {
+						document.write("<p>Aucun point de collecte trouvé</p>");
 					}
 				}
 			);
+			/**/
 		}
 	});
 }
--- hackatonServer/public/javascripts/geolocal.web.js
+++ hackatonServer/public/javascripts/geolocal.web.js
@@ -1,6 +1,6 @@
 function getLocation(f) {
-	f({coords: {latitude: 43.6465125, longitude: 3.7964135}});
-	/**
+	//f({coords: {latitude: 43.6465125, longitude: 3.7964135}});
+	/**/
 	if (navigator.geolocation) {
 		navigator.geolocation.getCurrentPosition(f);
 	} else {
@@ -9,3 +9,16 @@ function getLocation(f) {
 	//*/
 }
 
+function findDistance(c1, c2) {
+	var R = 6371; // km
+	var dLat = (c2.coords.latitude-c1.coords.latitude)/360*Math.PI;
+	var dLon = (c2.coords.longitude-c1.coords.longitude)/360*Math.PI;
+	var lat1 = c1.coords.latitude/360*Math.PI;
+	var lat2 = c2.coords.longitude/360*Math.PI;
+
+	var a = Math.sin(dLat/2) * Math.sin(dLat/2) +
+        Math.sin(dLon/2) * Math.sin(dLon/2) * Math.cos(lat1) * Math.cos(lat2); 
+	var c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1-a)); 
+	var d = R * c;
+	return d;
+}
